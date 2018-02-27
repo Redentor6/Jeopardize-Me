f04730ba0d5bbd514b8010b3ee9e6efac70d334d
Bradford Muron <bradfordmuron@ool-ad020304.dyn.optonline.net>
2018-02-27 16:16:05 -0500

    database push issues

--- models/jeopardizeme-DB.js
+++ models/jeopardizeme-DB.js
@@ -2,8 +2,12 @@ const pgp = require('pg-promise')();
 const dbConfig = require('../config/dbConfig');
 const db = pgp(dbConfig);  // db connection
 module.exports = {
-	save: function(thing) {
-		return db.one(`INSERT INTO...`, thing);
+	save: function(question) {
+		return db.one(`
+		INSERT INTO questions (question, answer, value)
+		VALUE $[question], $[answer], $[value]
+		RETURNING *
+		`, question);
 
 	},
 	delete: function() {
@@ -13,3 +17,11 @@ module.exports = {
 
 	}
 }
+
+module.exports.save({
+  question: "",
+  answer: "",
+  category: 'INTEGER'
+})
+.then(result => console.log(result))
+.catch(err => console.log(error.message));
